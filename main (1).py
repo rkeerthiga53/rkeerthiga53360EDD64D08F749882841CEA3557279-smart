994b042504f5cbb1e4d6c93c025e26586b8aff0f
rkeerthiga53 <146226526+rkeerthiga53@users.noreply.github.com>
2023-09-30 09:39:42 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+def fact_rec(n):
+
+if ne or n==1:
+return 1
+
+else:
+return n fact_rec (n-1)
+number-int(input("enter a value:"))
+res fact_rec(number) 9 print("the factorial of () is ().".format(number, res))
